21d3ce87101d952aa236f3398b317641bd0dc43b
Amir Hossein Akhavan <83725346+amir-akhavans@users.noreply.github.com>
2022-03-24 23:33:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,11 +113,6 @@ resource "aws_eks_cluster" "test" {
     subnet_ids = [var.subnet_id]
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-}
-
-
 
 
 
